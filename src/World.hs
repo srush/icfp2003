f0f462205be6f947ed6f2245a65ab8886576fc0b
srush <sasha.rush@gmail.com>
2009-06-13 13:52:42 -0700

    added the worl

---
+++ src/World.hs
@@ -0,0 +1,81 @@
+module World where 
+
+import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+import Text.ParserCombinators.Parsec.Language (emptyDef)
+import qualified Text.ParserCombinators.Parsec.Token as P 
+import Control.Applicative
+import Data.Array
+import Test.HUnit
+import Debug.Trace
+
+type Pos = (Int, Int) 
+data World = World {
+      ground :: Array Int (Array Int Square),
+      start :: Pos 
+} deriving Show
+
+-- Road True if it is finish
+data Square = Road Bool | Wall 
+              deriving Show
+
+
+getSquare ::  World -> Pos -> Square 
+getSquare world (x,y) = ((ground world) ! y) ! x
+
+isRoad :: World -> Pos -> Bool
+isRoad world pos  = 
+    case getSquare world pos of
+      Road _ -> True
+      _ -> False
+
+isBarrier :: World -> Pos -> Bool
+isBarrier world pos =  
+    case getSquare world pos of
+      Road _ -> False
+      _ -> True
+
+
+isGoal :: World -> Pos -> Bool
+isGoal world pos  = 
+    case getSquare world pos of
+      Road a -> a
+      _ -> False
+
+
+lexer = P.makeTokenParser emptyDef
+integer = P.integer lexer
+
+eol :: Parser Char
+eol = char '\n'
+
+fromFile :: String -> IO World
+fromFile filename = do
+  contents <- readFile filename
+  either (fail.show) (return.id)  $ parse readWorld "" contents
+
+
+readSquare :: Parser Square
+readSquare = 
+  Wall <$ oneOf "grwb" <|>
+  Road False <$ char '.' <|>
+  Road True  <$ char '!' <|>
+  Road False  <$ char '*' 
+  
+
+readWorld :: Parser World
+readWorld = do
+  w <- integer
+  h <- integer
+  grid <- sepBy (many readSquare) eol
+  let innerArrays = map (listArray (0, fromInteger (w-1))) grid 
+  let arrayForm =  listArray (0, fromInteger (h-1)) innerArrays
+  return $ World arrayForm  (0,0)
+
+testSimple = TestCase $ do 
+  world <- fromFile "/home/srush/Projects/icfp2003/data/supersimple.trk"
+  assertEqual "road fail" False $ isRoad world (0,0)
+  assertEqual "barrier check" True $ isBarrier world (0,0)
+  assertEqual "road success" True $ isRoad world (1,1) 
+  assertEqual "goal" True $ isGoal world (1,2) 
+
+worldTests = TestList $ [testSimple]
